7536ecb5cd46b6d0c0c4991d27d9ed2b1967b007
Noel <harrisbnoel@gmail.com>
2015-10-04 16:06:58 -0400

    master copy of project. Please create personal branch

---
+++ MidtermProject/Customer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermProject
+{
+    class Customer
+    {
+        // Add fields we agreed upon. Will be displayed in master copy.
+        public string custName;
+        public string phoneNum;
+        public string movieSelection;
+        public DateTime checkOut;
+
+
+        // Add properties we agreed upon. Will be displayed in master copy.
+        public string CustName
+        {
+            get
+            {
+                return this.custName;
+            }
+            set
+            {
+                this.custName = value;
+            }
+        }
+
+        public string PhoneNum
+        {
+            get
+            {
+                return this.PhoneNum;
+            }
+            set
+            {
+                this.PhoneNum = value;
+            }
+        }
+
+        public string MovieSelection
+        {
+            get
+            {
+                return this.movieSelection;
+            }
+            set
+            {
+                this.movieSelection = value;
+            }
+        }
+
+        public DateTime CheckOut
+        {
+            get
+            {
+                return this.checkOut;
+            }
+            set
+            {
+                this.checkOut = value;
+            }
+        }
+
+        // We will add our methods into our BRANCH copies of master.
+    }
+}
---
+++ MidtermProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We will add our main programs into our individual BRANCH copies of master.
+        }
+    }
+}
